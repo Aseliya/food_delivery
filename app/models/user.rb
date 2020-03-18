a935e6cba5c398beae5d9836ef710f4ea2045de3
Асель Кенжебекова <aselkenzebekova@MacBook-Air-Asel.local>
2020-03-19 00:15:59 +0600

    Rubocop checking

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
-	validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
 end
