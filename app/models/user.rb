63cb47bf02b2caa16abc0ba3401bfc292cd43ae9
Асель Кенжебекова <aselkenzebekova@MacBook-Air-Asel.local>
2020-03-19 12:49:00 +0600

    Add admin role to user and fix first user as an admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,18 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 50 }
+
+  after_create :set_admin
+
+  private
+
+  def set_admin
+    if User.count == 1
+      User.first.update_attribute(:is_admin, true)
+    else
+      true
+    end
+  end
 end
